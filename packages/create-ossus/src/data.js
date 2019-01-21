194c6aca11cefbbdaf0d30d5f4dd907d37b2a81d
Jared Jones <Jared.Jones@cdk.com>
2019-01-21 10:49:18 -0600

    updating versions on create-ossus script

--- packages/create-ossus/src/data.js
+++ packages/create-ossus/src/data.js
@@ -1,9 +1,9 @@
 module.exports = {
     packages: [
         { name: 'express', version: '^4.16.4' },
-        { name: 'ossus', version: '^1.0.3'},
-        { name: 'ossus-components', version: '^1.0.5'},
-        { name: 'ossus-scripts', version: '^1.0.1'},
+        { name: 'ossus', version: '^1.0.0'},
+        { name: 'ossus-components', version: '^1.0.0'},
+        { name: 'ossus-scripts', version: '^1.0.0'},
         { name: 'next', version: '^7.0.0'},
         { name: 'react', version: '^16.5.2'},
         { name: 'react-dom', version: '^16.5.2'},
